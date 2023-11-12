bb4732ee0bcaaf3b5c54460de55cadfbe6f8305e
Doug Bacelar <dougbacelar@gmail.com>
2023-11-12 18:04:02 +0900

    install telescope

--- lua/doug/packer.lua
+++ lua/doug/packer.lua
@@ -7,10 +7,13 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  -- fuzzy finding
   use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.4',
--- or                            , branch = '0.1.x',
-  requires = { {'nvim-lua/plenary.nvim'} }
-}
+	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use {
 end)
 
