feed84c10696e55934fc85113b40a354bb6b75f4
Doug Bacelar <dougbacelar@gmail.com>
2023-11-12 18:25:39 +0900

    add timeout settings

--- lua/doug/init.lua
+++ lua/doug/init.lua
@@ -1,4 +1,10 @@
-require("doug.remap")
-print("hello from doug")
+-- decrease update time, I think default is 4 seconds. should make plugins respond quicker
+vim.o.updatetime = 250
+-- decrease timeout for keybinds, should make shortcuts snappier but potentially harder to press.
+vim.o.timeoutlen = 300
 
+-- save undo history on disk even after closing file.
+vim.o.undofile = true
+
+require("doug.remap")
 
