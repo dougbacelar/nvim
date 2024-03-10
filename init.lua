37b12a77c02a0946224c56ccdf57650437fcd255
Doug Bacelar <dougbacelar@gmail.com>
2024-03-10 17:46:44 +0900

    add some extra options

--- init.lua
+++ init.lua
@@ -9,6 +9,24 @@ vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 -- save undo history on disk even after closing file.
 vim.o.undofile = true
+-- remove line numbers by default. Use :set number to manually turn it on and :set nonumber to manually turn it back off
+vim.opt.number = false
+-- tells nvim to use the "+" register. This will share the clipboard between nvim/MacOS
+vim.opt.clipboard = "unnamedplus"
+-- indents wrapped lines so they are easier to read
+vim.opt.breakindent = true
+-- makes search case-insensitive when all letters are lower case. If at least a single letter is upper case, the search will become case-sensitive
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+-- sets how neovim will display tabs, trailling spaces and non-breaking spaces in the editor. This helps identify wrong whitespaces inserted accidentally in a file
+--  See :help 'list'
+--  and :help 'listchars'
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+-- minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 8
+-- highlight current cursor line
+vim.opt.cursorline = true
 
 ----
 -- Bootstrap plugin manager
