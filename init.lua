4aa0f5845cf46db3850641a4b06e82619373d3e0
Doug Bacelar <dougbacelar@gmail.com>
2024-03-10 18:03:42 +0900

    add highlight on yank

--- init.lua
+++ init.lua
@@ -64,6 +64,18 @@ vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
 		end
 	end,
 })
+
+-- highlight when yanking (copying) text. Adjust `timeout` to change duration of highlight
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank({ timeout = 200 })
+	end,
+})
+
 ----
 -- Plugins
 ----
