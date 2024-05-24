de07487f57b03f2fae6749b19848a53dbbe1ff54
Doug Bacelar <dougbacelar@gmail.com>
2024-05-24 15:59:36 +0900

    move which-key to its own file

--- init.lua
+++ init.lua
@@ -541,25 +541,7 @@ require('lazy').setup {
     end,
   },
 
-  -- useful plugin to show you pending keybinds.
-  {
-    'folke/which-key.nvim',
-    event = 'VimEnter',
-    config = function()
-      require('which-key').setup()
-
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      }
-    end,
-  },
-
+  require 'plugins.which-key',
   require 'plugins.gitsigns',
   require 'plugins.nvim-treesitter-textobjects',
 }
