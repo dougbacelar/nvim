7d6e6852b3e5a58aaf46d751941dc5b8e4274add
Doug Bacelar <dougbacelar@gmail.com>
2024-05-26 19:21:26 +0900

    refactor init.lua

--- init.lua
+++ init.lua
@@ -1,11 +1,4 @@
--- make sure to set mapleader before lazy.nvim so the mappings are correct
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-----
--- Bootstrap plugin manager
-----
--- setup lazy.nvim to manage plugin dependencies
+-- bootstrap lazy.nvim to manage plugin dependencies
 -- See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
@@ -20,14 +13,11 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-----
--- Plugins
-----
---  To check the current status of your plugins, run
---    :Lazy
---  You can press `?` in this menu for help. Use `:q` to close the window
---  To update plugins, you can run
---    :Lazy update
+-- make sure to set mapleader before lazy.nvim so the mappings are correct
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- initialize plugins under plugins/ folder
 require('lazy').setup('plugins', {
   checker = {
     enabled = false, -- disable automatic update checks
@@ -44,6 +34,3 @@ require 'core.options'
 require 'core.netrw-config'
 require 'core.autocmds'
 require 'core.keymaps'
-
--- see `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
