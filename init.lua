1ec120925612f8b1afa68150099dbfa360d7d083
Doug Bacelar <dougbacelar@gmail.com>
2024-05-26 10:37:42 +0900

    move conform

--- init.lua
+++ init.lua
@@ -145,40 +145,6 @@ require('lazy').setup {
   -- required to to add filetype icons to netrw and telescope
   { 'kyazdani42/nvim-web-devicons' },
 
-  -- setup lsp formatters
-  {
-    'stevearc/conform.nvim',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      local conform = require 'conform'
-      conform.setup {
-        formatters_by_ft = {
-          lua = { 'stylua' },
-          -- conform will run multiple formatters sequentially
-          python = { 'isort', 'black' },
-          -- use a sub-list to run only the first available formatter
-          javascript = { { 'prettierd', 'prettier' } },
-          typescript = { { 'prettierd', 'prettier' } },
-          javascriptreact = { { 'prettierd', 'prettier' } },
-          typescriptreact = { { 'prettierd', 'prettier' } },
-          css = { { 'prettierd', 'prettier' } },
-          html = { { 'prettierd', 'prettier' } },
-          json = { { 'prettierd', 'prettier' } },
-          yaml = { { 'prettierd', 'prettier' } },
-          markdown = { { 'prettierd', 'prettier' } },
-          graphql = { { 'prettierd', 'prettier' } },
-        },
-        -- format on save is not working due to auto save, moved format code to auto-save autocmd
-        -- fomart_on_save = { lsp_fallback = true, async = false, timeout_ms = 500 },
-      }
-
-      -- keep this keymap as a fallback for now just in case the autocmd doesn't work
-      vim.keymap.set({ 'n', 'v' }, '<leader>l', function()
-        conform.format { async = false }
-      end, { desc = '[L]int' })
-    end,
-  },
-
   -- detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
@@ -203,6 +169,7 @@ require('lazy').setup {
   require 'plugins.telescope',
   require 'plugins.lsp-config',
   require 'plugins.cmp',
+  require 'plugins.conform',
 }
 
 -- modules in core are not loaded by Lazy
---
+++ lua/plugins/conform.lua
@@ -0,0 +1,33 @@
+-- setup lsp formatters
+return {
+  'stevearc/conform.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    local conform = require 'conform'
+    conform.setup {
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- conform will run multiple formatters sequentially
+        python = { 'isort', 'black' },
+        -- use a sub-list to run only the first available formatter
+        javascript = { { 'prettierd', 'prettier' } },
+        typescript = { { 'prettierd', 'prettier' } },
+        javascriptreact = { { 'prettierd', 'prettier' } },
+        typescriptreact = { { 'prettierd', 'prettier' } },
+        css = { { 'prettierd', 'prettier' } },
+        html = { { 'prettierd', 'prettier' } },
+        json = { { 'prettierd', 'prettier' } },
+        yaml = { { 'prettierd', 'prettier' } },
+        markdown = { { 'prettierd', 'prettier' } },
+        graphql = { { 'prettierd', 'prettier' } },
+      },
+      -- format on save is not working due to auto save, moved format code to auto-save autocmd
+      -- fomart_on_save = { lsp_fallback = true, async = false, timeout_ms = 500 },
+    }
+
+    -- keep this keymap as a fallback for now just in case the autocmd doesn't work
+    vim.keymap.set({ 'n', 'v' }, '<leader>l', function()
+      conform.format { async = false }
+    end, { desc = '[L]int' })
+  end,
+}
