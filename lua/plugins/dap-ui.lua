ef11a0e45927294d7b8737b87f6ce1331d7394d1
doug <dougbacelar@gmail.com>
2024-05-30 21:57:58 +0900

    update dap hotkeys

--- lua/plugins/dap-ui.lua
+++ lua/plugins/dap-ui.lua
@@ -123,37 +123,40 @@ return {
       },
     }
 
-
-    vim.keymap.set('n', '<leader>bb', "<cmd>lua require'dap'.toggle_breakpoint()<cr>")
-    vim.keymap.set('n', '<leader>bc', "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>")
-    vim.keymap.set('n', '<leader>bl', "<cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<cr>")
-    vim.keymap.set('n', '<leader>br', "<cmd>lua require'dap'.clear_breakpoints()<cr>")
-    vim.keymap.set('n', '<leader>ba', '<cmd>Telescope dap list_breakpoints<cr>')
-    vim.keymap.set('n', '<leader>dc', "<cmd>lua require'dap'.continue()<cr>")
-    vim.keymap.set('n', '<leader>dj', "<cmd>lua require'dap'.step_over()<cr>")
-    vim.keymap.set('n', '<leader>dk', "<cmd>lua require'dap'.step_into()<cr>")
-    vim.keymap.set('n', '<leader>do', "<cmd>lua require'dap'.step_out()<cr>")
+    vim.keymap.set('n', '<leader>db', "<cmd>lua require'dap'.toggle_breakpoint()<cr>", { desc = 'Toggle [D]ebug [B]reakpoint' })
+    vim.keymap.set('n', '<leader>di', "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>", { desc = '[D]ebug [I]f Condition' })
+    vim.keymap.set(
+      'n',
+      '<leader>dL',
+      "<cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<cr>",
+      { desc = '[D]ebug [L]og Message' }
+    )
+    vim.keymap.set('n', '<leader>dR', "<cmd>lua require'dap'.clear_breakpoints()<cr>", { desc = '[D]ebug [R]eset Breakpoints' })
+    vim.keymap.set('n', '<leader>dc', "<cmd>lua require'dap'.continue()<cr>", { desc = '[D]ebug [C]ontinue' })
+    vim.keymap.set('n', '<leader>dj', "<cmd>lua require'dap'.step_over()<cr>", { desc = '[D]ebug Step [J]over' })
+    vim.keymap.set('n', '<leader>di', "<cmd>lua require'dap'.step_into()<cr>", { desc = '[D]ebug [I]nto' })
+    vim.keymap.set('n', '<leader>do', "<cmd>lua require'dap'.step_out()<cr>", { desc = '[D]ebug [O]ut' })
     vim.keymap.set('n', '<leader>dd', function()
       require('dap').disconnect()
       require('dapui').close()
-    end)
+    end, { desc = '[D]ebug [D]isconnect' })
     vim.keymap.set('n', '<leader>dt', function()
       require('dap').terminate()
       require('dapui').close()
-    end)
-    vim.keymap.set('n', '<leader>dr', "<cmd>lua require'dap'.repl.toggle()<cr>")
-    vim.keymap.set('n', '<leader>dl', "<cmd>lua require'dap'.run_last()<cr>")
-    vim.keymap.set('n', '<leader>di', function()
+    end, { desc = '[D]ebug [T]erminate' })
+    vim.keymap.set('n', '<leader>dr', "<cmd>lua require'dap'.repl.toggle()<cr>", { desc = '[D]ebug [R]ead Eval Print Loop' })
+    vim.keymap.set('n', '<leader>d.', "<cmd>lua require'dap'.run_last()<cr>", { desc = '[D]ebug . (Last)' })
+    vim.keymap.set('n', '<leader>dk', function()
       require('dap.ui.widgets').hover()
-    end)
-    vim.keymap.set('n', '<leader>d?', function()
+    end, { desc = '[D]ebug [K] hover' })
+    vim.keymap.set('n', '<leader>ds', function()
       local widgets = require 'dap.ui.widgets'
       widgets.centered_float(widgets.scopes)
-    end)
-    vim.keymap.set('n', '<leader>df', '<cmd>Telescope dap frames<cr>')
-    vim.keymap.set('n', '<leader>dh', '<cmd>Telescope dap commands<cr>')
-    vim.keymap.set('n', '<leader>de', function()
-      require('telescope.builtin').diagnostics { default_text = ':E:' }
-    end)
+    end, { desc = '[D]ebug [S]copes' })
+    vim.keymap.set('n', '<leader>dl', '<cmd>Telescope dap list_breakpoints<cr>', { desc = '[D]ebug List Breakpoints' })
+    vim.keymap.set('n', '<leader>df', '<cmd>Telescope dap frames<cr>', { desc = '[D]ebug [F]rames' })
+    vim.keymap.set('n', '<leader>d:', '<cmd>Telescope dap commands<cr>', { desc = '[D]ebug : (Commands)' })
+    vim.keymap.set('n', '<leader>dC', "<cmd>lua require'jdtls'.test_class()<cr>", { desc = '[D]ebug [C]lass' })
+    vim.keymap.set('n', '<leader>dm', "<cmd>lua require'jdtls'.test_nearest_method()<cr>", { desc = '[D]ebug [M]ethod' })
   end,
 }
