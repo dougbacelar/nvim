8dbd710808d82b6e468e8d81648bb6d0e3bac5ea
Doug Bacelar <dougbacelar@gmail.com>
2024-06-01 22:40:06 +0900

    add window nav hotkeys

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,12 +7,11 @@ vim.keymap.set('n', '<esc>', '<cmd>noh<cr><esc>')
 
 -- toggle 'number'
 vim.keymap.set('n', '<leader>tn', ':set number!<CR>', { silent = true, noremap = true, desc = '[T]oggle [N]umber Lines' })
-
 -- toggle 'relativenumber'
 vim.keymap.set('n', '<leader>trn', ':set relativenumber!<CR>', { silent = true, noremap = true, desc = '[T]oggle [R]elative [N]umber Lines' })
 
 -- yank file name of current buffer
-vim.keymap.set('n', 'yN', ':let @+=expand("%:t")<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', 'yN', ':let @+=expand("%:t")<CR>', { noremap = true, silent = true, desc = '[Y]ank File [N]ame' })
 
 -- Buffers
 -- go to the previous buffer
@@ -21,3 +20,17 @@ vim.keymap.set('n', '<leader>pb', ':bprevious<CR>', { desc = '[P]revious [B]uffe
 vim.keymap.set('n', '<leader>nb', ':bnext<CR>', { desc = '[N]ext [B]uffer' })
 -- close the current buffer
 vim.keymap.set('n', '<leader>db', ':bdelete<CR>', { desc = '[D]elete [B]uffer' })
+
+-- windows
+-- easier focus navigation
+vim.keymap.set('n', '<c-j>', '<c-w><c-j>', { desc = 'Focus Below Window' })
+vim.keymap.set('n', '<c-k>', '<c-w><c-k>', { desc = 'Focus Above Window' })
+vim.keymap.set('n', '<c-l>', '<c-w><c-l>', { desc = 'Focus Right Window' })
+vim.keymap.set('n', '<c-h>', '<c-w><c-h>', { desc = 'Focus Left Window' })
+
+-- control window width
+vim.keymap.set('n', '<c-,>', '<c-w>5<', { desc = 'Decrese Window Width' })
+vim.keymap.set('n', '<c-.>', '<c-w>5>', { desc = 'Increase Window Width' })
+-- TODO: control window height
+-- vim.keymap.set("n", "<M-t>", "<C-W>+")
+-- vim.keymap.set("n", "<M-s>", "<C-W>-")
