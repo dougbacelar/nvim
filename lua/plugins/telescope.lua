6a3a22b604c81585eb3348aaa4901acc4abd6173
Doug Bacelar <dougbacelar@gmail.com>
2024-06-08 08:04:01 +0900

    handle long file paths better

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -41,10 +41,42 @@ return -- fuzzy finding files and stuff
       -- You can put your default mappings / updates / etc. in here
       --  All the info you're looking for is in `:help telescope.setup()`
       defaults = {
+        -- truncate long paths from the beginning
+        path_display = { 'truncate' },
+        -- increase width from 80% to 85% to fit longer paths.
+        layout_config = { width = 0.85 },
         mappings = {
           n = {
             ['dd'] = require('telescope.actions').delete_buffer,
             ['<leader>db'] = require('telescope.actions').delete_buffer,
+
+            -- <C-n>/<Down>	next item
+            -- <C-p>/<Up>	Previous item
+            -- j/k	        Next/previous (in normal mode)
+            -- H/M/L	        Select High/Middle/Low (in normal mode)
+            -- gg/G	        Select the first/last item (in normal mode)
+            -- <CR>	        Confirm selection
+            -- <C-x>	        Go to file selection as a split
+            -- <C-v>	        Go to file selection as a vsplit
+            -- <C-t>	        Go to a file in a new tab
+            -- <C-u>	        Scroll up in preview window
+            -- <C-d>	        Scroll down in preview window
+            -- <C-f>	        Scroll left in preview window
+            -- <C-k>	        Scroll right in preview window
+            -- <M-f>	        Scroll left in results window
+            -- <M-k>	        Scroll right in results window
+            -- <C-/>	        Show mappings for picker actions (insert mode)
+            -- ?	        Show mappings for picker actions (normal mode)
+            -- <C-c>	        Close telescope (insert mode)
+            -- <Esc>	        Close telescope (in normal mode)
+            -- <Tab>	        Toggle selection and move to next selection
+            -- <S-Tab>	        Toggle selection and move to prev selection
+            -- <C-q>	        Send all items not filtered to quickfixlist (qflist)
+            -- <M-q>	        Send all selected items to qflist
+            -- <C-r><C-w>	Insert cword in original window into prompt (insert mode)
+            -- <C-r><C-a>	Insert cWORD in original window into prompt (insert mode)
+            -- <C-r><C-f>	Insert cfile in original window into prompt (insert mode)
+            -- <C-r><C-l>	Insert cline in original window into prompt (insert mode)
           },
         },
       },
