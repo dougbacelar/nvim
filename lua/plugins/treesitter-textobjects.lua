74a9317f386f34d5ad03378db337f8480e284804
Doug Bacelar <dougbacelar@gmail.com>
2024-07-15 13:44:04 +0900

    add parameter textobjects

--- lua/plugins/treesitter-textobjects.lua
+++ lua/plugins/treesitter-textobjects.lua
@@ -58,6 +58,7 @@ return {
               [']c'] = { query = '@class.outer', desc = 'Next class start' },
               [']i'] = { query = '@conditional.outer', desc = 'Next conditional start' },
               [']l'] = { query = '@loop.outer', desc = 'Next loop start' },
+              [']a'] = { query = '@parameter.outer', desc = 'Next parameter start' },
 
               -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
               -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
@@ -71,6 +72,7 @@ return {
               [']C'] = { query = '@class.outer', desc = 'Next class end' },
               [']I'] = { query = '@conditional.outer', desc = 'Next conditional end' },
               [']L'] = { query = '@loop.outer', desc = 'Next loop end' },
+              [']A'] = { query = '@parameter.outer', desc = 'Next parameter start' },
             },
             goto_previous_start = {
               ['[f'] = { query = '@call.outer', desc = 'Prev function call start' },
@@ -78,6 +80,7 @@ return {
               ['[c'] = { query = '@class.outer', desc = 'Prev class start' },
               ['[i'] = { query = '@conditional.outer', desc = 'Prev conditional start' },
               ['[l'] = { query = '@loop.outer', desc = 'Prev loop start' },
+              ['[a'] = { query = '@parameter.outer', desc = 'Prev parameter start' },
             },
             goto_previous_end = {
               ['[F'] = { query = '@call.outer', desc = 'Prev function call end' },
@@ -85,6 +88,7 @@ return {
               ['[C'] = { query = '@class.outer', desc = 'Prev class end' },
               ['[I'] = { query = '@conditional.outer', desc = 'Prev conditional end' },
               ['[L'] = { query = '@loop.outer', desc = 'Prev loop end' },
+              ['[A'] = { query = '@parameter.outer', desc = 'Prev parameter start' },
             },
           },
         },
