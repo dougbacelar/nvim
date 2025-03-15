681c7917f47d22a58b1c2165eafb7371ef3e43bf
Doug Bacelar <dougbacelar@gmail.com>
2025-03-15 15:51:58 +0900

    make black

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -6,7 +6,14 @@ return {
   config = function()
     require('catppuccin').setup {
       flavour = 'mocha', -- latte, frappe, macchiato, mocha
-      transparent_background = true, -- disables setting the background color.
+      transparent_background = false, -- config that disables setting the background color.
+      color_overrides = {
+        mocha = {
+          base = '#000000',
+          mantle = '#000000',
+          -- crust = '#000000',
+        },
+      },
     }
     vim.cmd.colorscheme 'catppuccin-mocha'
   end,
