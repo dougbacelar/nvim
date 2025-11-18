602bd890d4445d36ad3c2a0dd48f2514af4583fe
Doug Bacelar <dougbacelar@gmail.com>
2025-11-18 14:05:59 +0900

    clean luasnip

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,26 +3,6 @@ return {
   'hrsh7th/nvim-cmp',
   event = 'InsertEnter',
   dependencies = {
-    -- snippet engine & its associated nvim-cmp source
-    {
-      'L3MON4D3/LuaSnip',
-      build = (function()
-        return 'make install_jsregexp'
-      end)(),
-      dependencies = {
-        -- `friendly-snippets` contains a variety of premade snippets.
-        --    See the README about individual language/framework/plugin snippets:
-        --    https://github.com/rafamadriz/friendly-snippets
-        -- {
-        --   'rafamadriz/friendly-snippets',
-        --   config = function()
-        --     require('luasnip.loaders.from_vscode').lazy_load()
-        --   end,
-        -- },
-      },
-    },
-    'saadparwaiz1/cmp_luasnip',
-
     -- adds other completion capabilities.
     --  nvim-cmp does not ship with all sources by default. They are split
     --  into multiple repos for maintenance purposes.
@@ -32,15 +12,8 @@ return {
   config = function()
     -- See `:help cmp`
     local cmp = require 'cmp'
-    local luasnip = require 'luasnip'
-    luasnip.config.setup {}
 
     cmp.setup {
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
       completion = { completeopt = 'menu,menuone,noinsert' },
 
       -- for an understanding of why these mappings were read `:help ins-completion`
@@ -63,26 +36,9 @@ return {
         -- manually trigger a completion from nvim-cmp.
         --  menerally not needed as nvim-cmp will display completions whenever they are available.
         ['<C-Space>'] = cmp.mapping.complete {},
-
-        -- <c-l> will move you to the right of each of the expansion locations.
-        -- <c-h> is similar, except moving you backwards.
-        ['<C-l>'] = cmp.mapping(function()
-          if luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          end
-        end, { 'i', 's' }),
-        ['<C-h>'] = cmp.mapping(function()
-          if luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          end
-        end, { 'i', 's' }),
-
-        -- for more advanced luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       },
       sources = {
         { name = 'nvim_lsp' },
-        { name = 'luasnip' },
         { name = 'path' },
       },
     }
