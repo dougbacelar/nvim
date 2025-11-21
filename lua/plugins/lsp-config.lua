bcd14314c5000cc6d456e13eefa0e1c8692d2d85
Doug Bacelar <dougbacelar@gmail.com>
2025-11-22 07:47:45 +0900

    revert breaking lsp change

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -81,7 +81,7 @@ return {
         -- by the server configuration above. Useful when disabling
         -- certain features of an LSP (for example, turning off formatting for tsserver)
         opts.capabilities = vim.tbl_deep_extend('force', {}, capabilities, opts.capabilities or {})
-        vim.lsp.config(server_name, opts)
+        require('lspconfig')[server_name].setup(opts)
       end
     end,
   },
